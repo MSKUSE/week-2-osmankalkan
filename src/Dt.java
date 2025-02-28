e65a8d6ae0304eb3fc9f0da5ca98b03d4e0c15fe
osmankalkan <okalkan@posta.mu.edu.tr>
2025-02-28 12:09:58 +0300

    Add files via upload

---
+++ src/Dt.java
@@ -0,0 +1,16 @@
+public class Dt {
+
+
+    public static void main(String[] args) {
+
+        int grade = 1;
+        double grade2 = 1.5;
+        boolean isOkay = true;
+        char first = 'A';
+
+        System.out.println(grade);
+        System.out.println(grade2);
+        System.out.println(isOkay);
+        System.out.println(first);
+    }
+}
